2df1e7cf1f9f10c4de326473b82c252b83222e20
PhillipFeldman <pfeldman@berkeley.edu>
2023-06-08 01:08:23 -0700

    tkinter starting

--- FileSearcher.py
+++ FileSearcher.py
@@ -1,20 +1,13 @@
 import os
+import tkinter as tk
 from pathlib import Path
 
-def main():
-    print()
-    path_pointer = Path.home()
-    print(path_pointer)
-    FileInfoPath = os.path.abspath("FileInfo")
-    print(FileInfoPath)
-    pointer_contents = os.scandir(path_pointer)
-    print(pointer_contents)
-    for i in pointer_contents:
-        print(i.path)
 
 
-    pass
 
 
-if __name__ == '__main__':
-    main()
+root = tk.Tk()
+root.geometry("500x500")
+root.title("File Searcher")
+
+root.mainloop()
--- test.py
+++ test.py
@@ -11,3 +11,14 @@ f.change_rating(4)
 f.remove_keyword("dog")
 f.add_keyword("bird")
 print(f.rating,f.keyword)
+
+def main():
+    print()
+    path_pointer = Path.home()
+    print(path_pointer)
+    FileInfoPath = os.path.abspath("FileInfo")
+    print(FileInfoPath)
+    pointer_contents = os.scandir(path_pointer)
+    print(pointer_contents)
+    for i in pointer_contents:
+        print(i.path)
