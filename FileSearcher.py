22e46ca5bc6a61d5ca6f55ba24b5fab8e052121d
PhillipFeldman <pfeldman@berkeley.edu>
2023-06-16 03:46:47 -0700

    Can now start searching by keywords
    Was very difficult, lots of scope issues

--- FileSearcher.py
+++ FileSearcher.py
@@ -4,12 +4,22 @@ Such as an accent over a vowel in Spanish,  I get the following character �.
 Pycharm suggests I change encoding to  windows-1252.
 I don't know if that will be an issue.  EG, when I convert to executables, what about other languages, etc.
 If there is a 'universal' encoding for all characters, will that slow down the program?"""
-
+import os
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
 from FileObject import FileObject
+from pathlib import Path
+
+
+class Search_Store():
+    def __init__(self):
+        self.keyword_set = set({})
+        self.search_all = False
+        self.rating_set = set({})
+        self.search_by_keyword = False
+
 
 # create the root window
 root = tk.Tk()
@@ -164,8 +174,107 @@ def file_explorer():
 
     window.mainloop()
 
+def keyword_frame(window,keyword_var,search_store):
+
+    if (keyword_var):
+        print("keyword checked")
+    else:
+        print("unchecked")
+    search_store.search_by_keyword = keyword_var
+    print(search_store.search_by_keyword)
+
+
+def add_remove_keyword(var,keyword_set,keyword,search_store):
+    var.set(not var.get())
+    if var.get():
+        keyword_set.add(keyword)
+    else:
+        keyword_set.remove(keyword)
+
+
+    search_store.keyword_set = keyword_set
+    print(keyword_set)
+
+def create_keyword_check_lambda(var,keyword_set,keyword,search_store):
+    return lambda: add_remove_keyword(var,keyword_set,keyword,search_store)
+
+
+def create_keywords_frame(window,search_store):
+    keywords_frame = tk.Frame(window)
+    keywords_frame.grid(row=1, column=0)
+    #######
+    checked_keywords_set = set({})
+    keyword_path_str = "./FileInfo/Associations/KeywordAssociations/"
+
+    path_pointer = Path(keyword_path_str)
+    pointer_contents = os.scandir(path_pointer)
+    root.title("File Searcher")
+    check_boxes = []
+    pc = list(pointer_contents)
+    paths = [i.path for i in pc]
+    lambdas = []
+    keyword_checked_vars = []
+    keywords = []
+
+    for i in range(len(paths)):
+        v = tk.BooleanVar()
+        keyword_checked_vars.append(v)
+        p = paths[i]
+        this_keyword = p[len(keyword_path_str) - 2:p.find('.txt')]
+        keywords.append(this_keyword)
+        lambdas.append(create_keyword_check_lambda(v, checked_keywords_set, this_keyword,search_store))
+
+    for i in range(len(pc)):
+        b = tk.Checkbutton(keywords_frame, text=keywords[i], \
+                           command=lambdas[i], \
+                           variable=keyword_checked_vars[i], \
+                           onvalue=True, offvalue=False)
+        b.var = keyword_checked_vars[i]
+        b.pack()
+        check_boxes.append(b)
+
+    #######
+    keyword_var = tk.BooleanVar()
+
+    def change_keyword_var(window, keyword_var,search_store):
+        keyword_var.set(not keyword_var.get())
+        keyword_frame(window, keyword_var.get(),search_store)
+
+    keyword_check = tk.Checkbutton(window, text="Search by Keywords", \
+                                   command=lambda: change_keyword_var(window, keyword_var,search_store), \
+                                   variable=keyword_var, \
+                                   onvalue=True, offvalue=False)
+
+    keyword_check.var = keyword_var
+    keyword_check.grid(row=0, column=0)
+
+def search(search_store):
+    file_set = set({})
+    if search_store.search_all == False:
+        if search_store.search_by_keyword:
+            for keyword in search_store.keyword_set:
+                with open(f'./FileInfo/Associations/KeywordAssociations/{keyword}.txt','r') as f:
+                    for line in f.readlines():
+                        file_set = file_set.union(line[1:-2])
+    paths = []
+    for num in list(file_set):
+        with open(f'./FileInfo/FileNums/{num}.txt', 'r') as f:
+            paths.append(f.readline()[5:-1])
+    print(paths)
+
 def file_searcher():
     print("searching")
+    window = tk.Tk()
+    search_store = Search_Store()
+    create_keywords_frame(window,search_store)
+
+    search_button = tk.Button(window,text="Search",command=lambda:search(search_store))
+    search_button.grid(row = 2, column=0)
+
+
+
+    window.mainloop()
+
 
 # file_explorer button
 file_explorer_button = ttk.Button(
